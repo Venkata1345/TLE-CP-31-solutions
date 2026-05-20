25341e1aded046d76448f03c3b796a9b9ca3b47f
Venkata Abhishek <venkataabhishek054@gmail.com>
2026-05-20 18:13:49 -0500

    Add initial implementation of number processing script

---
+++ Table with numbers.py
@@ -0,0 +1,96 @@
+from __future__ import division, print_function
+
+import os
+import sys
+from __builtin__ import xrange as range
+from cStringIO import StringIO
+from future_builtins import ascii, filter, hex, map, oct, zip
+from io import IOBase
+
+import __pypy__
+
+
+def main():
+    for _ in range(int(input())):
+        n,h,l = map(int, input().split())
+        arr= list(map(int, input().split()))
+        r=0
+        c=0
+        valid = 0
+        max_limit= max(h,l)
+    
+        for x in arr:
+            if x<=h:
+                r+=1
+            if x<=l:
+                c+=1
+            if x<=max_limit:
+                valid+=1
+        ans = min(min(r,c),valid//2)
+    print(ans)
+
+
+# region fastio
+
+BUFSIZE = 8192
+
+
+class FastI(IOBase):
+    def __init__(self, file):
+        self._file = file
+        self._fd = file.fileno()
+        self._buffer = StringIO()
+        self.newlines = 0
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count("\n") + (not b)
+            ptr = self._buffer.tell()
+            self._buffer.seek(0, 2), self._buffer.write(b), self._buffer.seek(ptr)
+        self.newlines -= 1
+        return self._buffer.readline()
+
+
+class FastO(IOBase):
+    def __init__(self, file):
+        self._file = file
+        self._fd = file.fileno()
+        self._buffer = __pypy__.builders.StringBuilder()
+        self.write = lambda s: self._buffer.append(s)
+
+    def flush(self):
+        os.write(self._fd, self._buffer.build())
+        self._buffer = __pypy__.builders.StringBuilder()
+
+
+def print(*args, **kwargs):
+    sep, file = kwargs.pop("sep", " "), kwargs.pop("file", sys.stdout)
+    at_start = True
+    for x in args:
+        if not at_start:
+            file.write(sep)
+        file.write(str(x))
+        at_start = False
+    file.write(kwargs.pop("end", "\n"))
+    if kwargs.pop("flush", False):
+        file.flush()
+
+
+sys.stdin, sys.stdout = FastI(sys.stdin), FastO(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+# endregion
+
+if __name__ == "__main__":
+    main() 
